f643a160101334ea97adaa0ccc7cab3d9db9ada4
TAGOMORI Satoshi <tagomoris@gmail.com>
2020-11-13 15:13:20 +0900

    implemented _[] and additional key-value pairs

--- lib/binding/slicer.rb
+++ lib/binding/slicer.rb
@@ -1,12 +1,12 @@
 class Binding
   module Slicer
     module Mixin
-      def slice(*symbols)
+      def slice(*symbols, **additionals)
         hash = {}
         symbols.each do |name|
           hash[name] = local_variable_get(name)
         end
-        hash
+        hash.merge(additionals)
       end
 
       alias :"[]" :slice
@@ -14,6 +14,19 @@ class Binding
 
     refine Binding do
       include Binding::Slicer::Mixin
+    end
+  end
+
+  module SSlicer
+    module Mixin
+      alias :_ :binding
+    end
+
+    refine Binding do
+      include Binding::Slicer::Mixin
+    end
+    refine Kernel do
+      include Binding::SSlicer::Mixin
     end
   end
 end
